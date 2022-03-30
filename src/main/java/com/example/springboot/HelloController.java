4ad38a11f2763e23f01d44fbf63e25808031c623
nitashav-vmw <nitashav@vmware.com>
2022-03-30 10:44:03 -0400

    Live update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!";
+		return "Live Update: Greetings from Spring Boot + Tanzu!";
 	}
 	
 }
